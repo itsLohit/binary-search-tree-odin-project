c65e13995f10b0876818cac9455f4d9d444c17e5
itsLohit <109834876+itsLohit@users.noreply.github.com>
2025-05-18 21:05:16 +0530

    Completed the Binary Search Tree.

---
+++ test.js
@@ -0,0 +1,29 @@
+import { Tree } from "./binary-tree.js";
+
+const randomArray = (size) => {
+  return Array.from({ length: size }, () => Math.floor(Math.random() * 100));
+}
+
+const tree = new Tree(randomArray(20));
+
+console.log(tree.isBalanced());
+
+console.log(tree.levelOrder());
+console.log(tree.inorder());
+console.log(tree.preorder());
+console.log(tree.postorder());
+
+tree.insert(300);
+tree.insert(400);
+tree.insert(500);
+
+console.log(tree.isBalanced());
+tree.rebalance();
+console.log(tree.isBalanced());
+
+console.log(tree.levelOrder());
+console.log(tree.inorder());
+console.log(tree.preorder());
+console.log(tree.postorder());
+
+tree.prettyPrint();
